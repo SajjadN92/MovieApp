30940b4412ab61d01318ae488b1ac134666d5fb2
Sajjad <SajjadN92@live.com>
2024-07-25 01:50:33 +0330

    Added gitignore

--- Movie App/SupportingFiles/SceneDelegate.swift
+++ Movie App/SupportingFiles/SceneDelegate.swift
@@ -14,9 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let vc = UIViewController()
-        vc.view.backgroundColor = .red
-        window?.rootViewController = vc
+        window?.rootViewController = UIViewController()
         window?.makeKeyAndVisible()
     }
 
